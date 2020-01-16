04d25ae4526e32872210826c4989b58d3a2b9671
Peng <pengxu.he@happyelements.com>
2020-01-16 12:51:20 +0800

    re-create

--- routing-service/src/main/java/com/peng/skeleton/routingservice/RoutingServiceApplication.java
+++ routing-service/src/main/java/com/peng/skeleton/routingservice/RoutingServiceApplication.java
@@ -2,9 +2,20 @@ package com.peng.skeleton.routingservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping(path = "/routing")
 @SpringBootApplication
 public class RoutingServiceApplication {
+
+	@GetMapping(path = "/get")
+	public String getRecommendation() {
+		return "hello world peng";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoutingServiceApplication.class, args);
